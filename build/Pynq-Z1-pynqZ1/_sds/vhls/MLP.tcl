b60bb20cb2e18e345ec0b8904fa59e404e0c125e
theDweeb <sfurgurs@uncc.edu>
2019-08-14 18:03:18 -0400

    fixed gitignore

--- build/Pynq-Z1-pynqZ1/_sds/vhls/MLP.tcl
+++
@@ -1,7 +0,0 @@
-set_directive_interface -mode ap_fifo "MLP" out
-set_directive_interface -mode ap_fifo "MLP" l2_b
-set_directive_interface -mode ap_fifo "MLP" image
-set_directive_interface -mode ap_fifo "MLP" l1_b
-set_directive_interface -mode ap_fifo "MLP" l1_w
-set_directive_interface -mode ap_fifo "MLP" l2_w
-set_directive_latency -min 1 MLP
--- build/Pynq-Z1-pynqZ1/pynqlib.d
+++
@@ -1,6 +0,0 @@
-/home/steve/Graduate_Research/MLP/build/Pynq-Z1-pynqZ1/pynqlib.o \
- /home/steve/Graduate_Research/MLP/build/Pynq-Z1-pynqZ1/pynqlib.o: \
- /home/steve/Graduate_Research/MLP/PynqLib/pynqlib.c \
- /home/steve/Graduate_Research/MLP/PynqLib/libxlnk_cma.h
-
-/home/steve/Graduate_Research/MLP/PynqLib/libxlnk_cma.h:
